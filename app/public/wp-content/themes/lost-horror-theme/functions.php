7c0705809f9fe91adff9ced6fa7ef667c93fc462
Steven-Klavins <stevenklavins94@gmail.com>
2020-10-13 18:32:10 +0100

    chore: added functions file to load resources

---
+++ app/public/wp-content/themes/lost-horror-theme/functions.php
@@ -0,0 +1,11 @@
+<?php 
+
+function lost_horror_files() {
+    wp_enqueue_style('lost_horror_main_styles', get_stylesheet_uri());  
+}
+
+add_action('wp_enqueue_scripts', 'lost_horror_files');
+
+
+
+?>
