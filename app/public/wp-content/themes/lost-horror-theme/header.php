f1084a6af611bcda1e7ad72685fe7ca49f6b19cf
Steven-Klavins <stevenklavins94@gmail.com>
2020-10-15 00:22:32 +0100

    feat: added mobile menu

--- app/public/wp-content/themes/lost-horror-theme/header.php
+++ app/public/wp-content/themes/lost-horror-theme/header.php
@@ -8,12 +8,27 @@
 <body>
     <div class="navHeader">
         <h1><span>Lost</span>Horror</h1>
+
         <div class="navLinks">
             <a class="active" href="#home">Home</a>
             <a href="#news">News</a>
             <a href="#contact">Movie Reviews</a>
             <a href="#practicaleffects">Practical Effects</a>
             <a href="#thearchive">The Archive</a>
-
             </div>
+            <div >
+                <label class="burger" for = "toggle">&#9776;</label>
+                <input type="checkbox" id ="toggle"/>
+                <span class="sideMenu">
+                <a href="#">Home</a>
+                    <a href="#">News</a>
+                    <a href="#">Movie Reviews</a>
+                    <a href="#">Practical Effects</a>
+                    <a href="#">The Archive</a>
+                    </span>
+
+        </div>
+
+
     </div>
+
