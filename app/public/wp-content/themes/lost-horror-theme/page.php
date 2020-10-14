0e8b8d9a88a7c0a057198496a976cb5116bcb98c
Steven-Klavins <stevenklavins94@gmail.com>
2020-10-14 16:45:47 +0100

    chore: refactored page

--- app/public/wp-content/themes/lost-horror-theme/page.php
+++ app/public/wp-content/themes/lost-horror-theme/page.php
@@ -1,10 +1,10 @@
-<?php 
+<?php
 get_header();
-while(have_posts()){
-    the_post(); ?>
+while (have_posts()) {
+    the_post();?>
     <h1>This is the page placeholder</h1>
-    <h2>  <?php the_title() ?></h2>
-    <?php the_content(); ?>
+    <h2>  <?php the_title()?></h2>
+    <?php the_content();?>
     <?php
 }
 
