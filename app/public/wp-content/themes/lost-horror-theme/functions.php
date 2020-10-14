6a401ab88245ee32ba7c3faef2aae9cddb4b4a4e
Steven-Klavins <stevenklavins94@gmail.com>
2020-10-14 16:45:11 +0100

    chore: refactored functions

--- app/public/wp-content/themes/lost-horror-theme/functions.php
+++ app/public/wp-content/themes/lost-horror-theme/functions.php
@@ -1,11 +1,8 @@
-<?php 
+<?php
 
-function lost_horror_files() {
-    wp_enqueue_style('lost_horror_main_styles', get_stylesheet_uri());  
+function lost_horror_files()
+{
+    wp_enqueue_style('lost_horror_main_styles', get_stylesheet_uri());
 }
 
 add_action('wp_enqueue_scripts', 'lost_horror_files');
-
-
-
-?>
