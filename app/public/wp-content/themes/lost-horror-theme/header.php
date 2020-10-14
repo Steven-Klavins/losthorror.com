5f522eebaf605fabdf588b064f8254cbe36646e6
Steven-Klavins <stevenklavins94@gmail.com>
2020-10-14 16:45:30 +0100

    chore: added place holder links

--- app/public/wp-content/themes/lost-horror-theme/header.php
+++ app/public/wp-content/themes/lost-horror-theme/header.php
@@ -1,9 +1,17 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <?php wp_head(); ?> 
-    </head>
-    <body>
-      <h1>Lost Horror</h1>  
 
-    
+<head>
+    <?php wp_head();?>
+</head>
+
+<body>
+    <div class="navHeader">
+        <h1><span>Lost</span>Horror</h1>
+        <div class="navLinks">
+            <a class="active" href="#home">Home</a>
+            <a href="#news">News</a>
+            <a href="#contact">Contact</a>
+            <a href="#about">About</a>
+            </div>
+    </div>
