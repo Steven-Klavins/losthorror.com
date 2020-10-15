148b0e349f87f90e7f841120ea474553fd80e714
Steven-Klavins <stevenklavins94@gmail.com>
2020-10-15 16:50:31 +0100

    Feat: set blog images to card backgrounds

--- app/public/wp-content/themes/lost-horror-theme/index.php
+++ app/public/wp-content/themes/lost-horror-theme/index.php
@@ -1,11 +1,16 @@
 <?php
 get_header();
 ?>
+<h2>Welcome!</h2>
 <?php
 while (have_posts()) {
     ?>
-    <div class="postCard">
+
+
         <?php the_post();?>
+        <?php $thumb = get_the_post_thumbnail_url(get_the_ID(), 'full');?>
+        <div class="postCard" style="background-image: url('<?php echo $thumb; ?>') ">
+
         <h2><?php the_title()?></h2>
         <?php the_content();?>
         <a href="<?php the_permalink();?>">Read more...</a>
