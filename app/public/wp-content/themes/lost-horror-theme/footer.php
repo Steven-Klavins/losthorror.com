cfef1e7ee56ca71103d0b75c1bb1d4a633bd08af
Steven-Klavins <stevenklavins94@gmail.com>
2020-10-15 01:52:03 +0100

    chore: added social logos to footer

--- app/public/wp-content/themes/lost-horror-theme/footer.php
+++ app/public/wp-content/themes/lost-horror-theme/footer.php
@@ -1,5 +1,12 @@
-<p>Footer place holder</p>
+<div class="footer">
+<div class="footerLogos">
+    <span><a href="instagram.com">J</a></span>
+    <span><a href="twitter.com">Q</a></span>
+    <span><a href="facebook.com">G</a></span>
+    <span><a href="youtube.com">W</a></span>
+</div>
+</div>
 
-<?php wp_footer(); ?> 
-</body> 
+<?php wp_footer();?>
+</body>
 </html>
