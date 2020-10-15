38fb27e2aff9b52a67066e66058df018b2c59112
Steven-Klavins <stevenklavins94@gmail.com>
2020-10-15 12:55:31 +0100

    fix: amended posts loop

--- app/public/wp-content/themes/lost-horror-theme/index.php
+++ app/public/wp-content/themes/lost-horror-theme/index.php
@@ -1,21 +1,16 @@
 <?php
 get_header();
 ?>
-
-
 <?php
 while (have_posts()) {
     ?>
     <div class="postCard">
-    <h2><?php the_title()?></h2>
-
-    <?php the_post();?>
-
-
-    <?php the_content();?>
-<a href="<?php the_permalink();?>">Read more...</a>
-</div>
-    <?php
+        <?php the_post();?>
+        <h2><?php the_title()?></h2>
+        <?php the_content();?>
+        <a href="<?php the_permalink();?>">Read more...</a>
+    </div>
+<?php
 
 }
 
