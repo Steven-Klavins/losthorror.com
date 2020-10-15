ebff01add91adce9af159cc5d2340f05c2577df7
Steven-Klavins <stevenklavins94@gmail.com>
2020-10-15 12:25:19 +0100

    chore: refactored html/php

--- app/public/wp-content/themes/lost-horror-theme/index.php
+++ app/public/wp-content/themes/lost-horror-theme/index.php
@@ -1,13 +1,22 @@
 <?php
 get_header();
 ?>
-<h2><?php the_title()?></h2>
+
+
 <?php
 while (have_posts()) {
-    the_post();?>
+    ?>
+    <div class="postCard">
+    <h2><?php the_title()?></h2>
+
+    <?php the_post();?>
+
 
     <?php the_content();?>
+<a href="<?php the_permalink();?>">Read more...</a>
+</div>
     <?php
+
 }
 
 get_footer();
