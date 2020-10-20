b42ebb3f4d8daa46e1d1bd4d01767fd8aa9f569f
Steven-Klavins <stevenklavins94@gmail.com>
2020-10-20 16:34:09 +0100

    chore: updated burger menu links

--- app/public/wp-content/themes/lost-horror-theme/header.php
+++ app/public/wp-content/themes/lost-horror-theme/header.php
@@ -1,6 +1,5 @@
 <!DOCTYPE html>
 <html>
-
 <head>
     <?php wp_head();?>
 </head>
@@ -17,11 +16,7 @@
             <label class="burger" for="toggle">&#9776;</label>
             <input type="checkbox" id="toggle" />
             <span class="sideMenu">
-                <a href="<?php echo site_url() ?>">Home</a>
-                <a href="<?php echo site_url('/news') ?>">News</a>
-                <a href="<?php echo site_url('/movie-reviews') ?>">Movie Reviews</a>
-                <a href="<?php echo site_url('/practical-effects') ?>">Practical Effects</a>
-                <a href="<?php echo site_url('/the-archive') ?>">The Archive</a>
+                <?php wp_nav_menu(array('theme_location' => 'headerMenuLocation'));?>
             </span>
 
         </div>
