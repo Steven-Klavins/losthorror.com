5166085cdaa5aa0bdc39d71a8a27d8a1b4df9d45
Steven-Klavins <stevenklavins94@gmail.com>
2020-10-20 16:31:28 +0100

    chore: updated nav links

--- app/public/wp-content/themes/lost-horror-theme/header.php
+++ app/public/wp-content/themes/lost-horror-theme/header.php
@@ -10,11 +10,8 @@
         <h1><span>Lost</span>Horror</h1>
 
         <div class="navLinks">
-            <a class="active" href="<?php echo site_url() ?>">Home</a>
-            <a href="<?php echo site_url('/news') ?>">News</a>
-            <a href="<?php echo site_url('/movie-reviews') ?>">Movie Reviews</a>
-            <a href="<?php echo site_url('/practical-effects') ?>">Practical Effects</a>
-            <a href="<?php echo site_url('/the-archive') ?>">The Archive</a>
+
+            <?php wp_nav_menu(array('theme_location' => 'headerMenuLocation'));?>
         </div>
         <div>
             <label class="burger" for="toggle">&#9776;</label>
