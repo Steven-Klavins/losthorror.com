1471923e4a26dae65583b7113f8fe667f78a7c83
Steven-Klavins <stevenklavins94@gmail.com>
2020-10-22 20:27:46 +0100

    feat: added custom page theme for reviews page

---
+++ app/public/wp-content/themes/lost-horror-theme/movie-reviews-custom-template.php
@@ -0,0 +1,32 @@
+
+<?php
+/*
+Template Name: Reviews Page
+ */
+get_header();
+?>
+<div class="scroll">
+<h2>Latest Reviews</h2>
+<?php
+while (have_posts()) {
+    ?>
+    <?php the_post();?>
+    <?php $thumb = get_the_post_thumbnail_url(get_the_ID(), 'full');?>
+    <div class="postCard" style="background-image: url('<?php echo $thumb; ?>') ">
+
+        <h2><?php the_title()?></h2>
+        <?php the_content();?>
+        <a href="<?php the_permalink();?>">Read more...</a>
+    </div>
+<?php
+
+}
+
+?>
+
+</div>
+<?php
+
+get_footer();
+
+?>
