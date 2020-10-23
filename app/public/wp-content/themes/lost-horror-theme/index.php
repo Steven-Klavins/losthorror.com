5c01b473742722dc16438f120ac1e3338c993cba
Steven-Klavins <stevenklavins94@gmail.com>
2020-10-24 00:38:49 +0100

    feat: added custom page pagination

--- app/public/wp-content/themes/lost-horror-theme/index.php
+++ app/public/wp-content/themes/lost-horror-theme/index.php
@@ -5,17 +5,22 @@ get_header();
 
 <h2>Latest Reviews </h2>
 <?php
-$the_query = query_posts(array(
-    'category_name' => 'movie-reviews', 'posts_per_page' => -1,
-));
 
-$postslist = get_posts($the_query);
+$default_posts_per_page = get_option('posts_per_page');
 
-foreach ($postslist as $post) {
-    if (in_category('movie-reviews')) {
-        ?>
-    <?php the_post();?>
-    <?php $thumb = get_the_post_thumbnail_url(get_the_ID(), 'full');?>
+$args = array(
+    'category_name' => 'movie-reviews',
+    'orderby' => 'date',
+    'order' => 'DESC',
+    'posts_per_page' => $default_posts_per_page,
+    'paged' => $paged,
+);
+$posts = query_posts($args);
+
+while (have_posts()) {
+    the_post();
+
+    $thumb = get_the_post_thumbnail_url(get_the_ID(), 'full');?>
     <div class="postCard" style="background-image: url('<?php echo $thumb; ?>') ">
 
         <h2><?php the_title()?></h2>
@@ -23,10 +28,17 @@ foreach ($postslist as $post) {
         <a href="<?php the_permalink();?>">Read more...</a>
     </div>
 <?php
-}
 
 }
 
+$wp_query = new WP_Query($args);
+$big = 999999999;
+echo paginate_links(array(
+    'base' => str_replace($big, '%#%', esc_url(get_pagenum_link($big))),
+    'format' => '?paged=%#%',
+    'current' => max(1, get_query_var('paged')),
+    'total' => $wp_query->max_num_pages,
+));
 ?>
 </div>
 <?php
