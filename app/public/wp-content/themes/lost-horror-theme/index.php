7b5f906b30aa7c42cae395b705d66f9b668af16e
Steven-Klavins <stevenklavins94@gmail.com>
2020-10-24 01:21:43 +0100

    chore: added pagination class

--- app/public/wp-content/themes/lost-horror-theme/index.php
+++ app/public/wp-content/themes/lost-horror-theme/index.php
@@ -6,19 +6,19 @@ get_header();
 <h2>Latest Reviews </h2>
 <?php
 
-$default_posts_per_page = get_option('posts_per_page');
+$default_posts_per_page = get_option('posts_per_page'); // Retrieve user set posts per page
 
 $args = array(
     'category_name' => 'movie-reviews',
     'orderby' => 'date',
     'order' => 'DESC',
-    'posts_per_page' => $default_posts_per_page,
+    'posts_per_page' => $default_posts_per_page, // Set posts per page to user set value
     'paged' => $paged,
 );
-$posts = query_posts($args);
+$posts = query_posts($args); // pass the query arguments including category_name to get the relevent posts
 
 while (have_posts()) {
-    the_post();
+    the_post(); // Loop through posts retrieved in desired format
 
     $thumb = get_the_post_thumbnail_url(get_the_ID(), 'full');?>
     <div class="postCard" style="background-image: url('<?php echo $thumb; ?>') ">
@@ -31,13 +31,16 @@ while (have_posts()) {
 
 }
 
-$wp_query = new WP_Query($args);
-$big = 999999999;
+$wp_query = new WP_Query($args); // Create a WP_Query with the previous arguments
+$big = 999999999; // Set a large number to evaluate all posts
+?>
+<div class="pagination">
+<?php
 echo paginate_links(array(
     'base' => str_replace($big, '%#%', esc_url(get_pagenum_link($big))),
     'format' => '?paged=%#%',
     'current' => max(1, get_query_var('paged')),
-    'total' => $wp_query->max_num_pages,
+    'total' => $wp_query->max_num_pages, // Set custom arugments as follows, and pass the WP_Query to max_num_pages
 ));
 ?>
 </div>
